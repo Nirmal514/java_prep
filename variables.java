9b5e0f8d3f115f68bf7f5fdb3aff2fc7c61f2103
Nirmal514 <seervinirmal40@gmail.com>
2026-03-08 15:50:43 +0530

    Added Java variables practice

---
+++ variables.java
@@ -0,0 +1,13 @@
+public class VariableDemo {
+    public static void main(String[] args) {
+        int age = 21;
+        double salary = 25000.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(isStudent);
+    }
+}
