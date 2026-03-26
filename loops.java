faf996cead72fbef307afda60f56aecbd015ac70
Nirmal514 <seervinirmal40@gmail.com>
2026-03-26 18:37:13 +0530

    Added Java loop practice code

---
+++ loops.java
@@ -0,0 +1,37 @@
+# for loop example in Java
+
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
+
+# while loop example in Java
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("Number: " + i);
+            i++;
+        }
+    }
+}
+
+# do-while loop example in Java
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println("Number: " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
+
